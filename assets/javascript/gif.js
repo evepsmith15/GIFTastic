990f46b7fda7b368d4d9fd3d0bd834346626f3be
evepsmith15 <50637882+evepsmith15@users.noreply.github.com>
2019-06-20 23:37:58 -0700

    Add files via upload

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,39 +1,40 @@
 function AnimalButton() {
-        var userInput = $('#animal-input').val();
-        button(userInput);
-    }
-    
+    var userInput = $('#animal-input').val();
+    results(userInput);
+}
+
 function submitButton() { //when submit is pressed, a new button is added
     var userInput = $('#animal-input').val();
     if (userInput) {
-        $('#animal-buttons').append("<button type='button' onclick='results(\"" + userInput + "\")' class='btn btn-primary' value=' " + userInput + "'> " + userInput + " </button>");
+        $('#animal-buttons').append("<button type='results' onclick= 'results(\"" + userInput + "\")'  class='btn btn-primary' value=' " + userInput + "'> " + userInput + " </button>");
     }
-
-$("button").on("click", function () { //this code will have the button when you click the tag 
-    var animal = $(this).attr("data-animal");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        animal + "&api_key=dc6zaTOxFJmzC&limit=10";
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) { 
-        console.log(response);
-        var results = response.data;
-        for (var i = 0; i < results.length; i++) {
-            var animalDiv = $("<div>"); //empty div
-            var p = $('<p>').text("rating: " + results[i].rating); //displays rating
-            var image = $("<img>");
-            image.attr("src", results[i].images.fixed_height.url); //displays image
-            animalDiv.append(p);
-            animalDiv.append(image);
-            $("#gifs-appear-here").prepend(animalDiv);
-        }
-    });
-});
-
 }
 
-$('.movImage').on('click', function() { //this code allows the gif to move and freeze
+function results(animal) {
+    $("button").on("click", function () { //this code will have the button when you click the tag 
+       // var animal = $(this).attr("#animal-input");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            animal + "&api_key=dc6zaTOxFJmzC&limit=10";
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            var results = response.data;
+            console.log(response);
+            for (var i = 0; i < results.length; i++) {
+                var animalDiv = $("<div>"); //empty div
+                var p = $('<p>').text("rating: " + results[i].rating); //displays rating
+                var image = $("<img>");
+                image.attr("src", results[i].images.fixed_height.url); //displays image
+                animalDiv.append(p);
+                animalDiv.append(image);
+                $("#gifs-appear-here").prepend(animalDiv);
+            }
+        });
+    });
+}
+
+$('.movImage').on('click', function () { //this code allows the gif to move and freeze
     var state = $(this).attr('data-state');
     if (state == 'still') {
         $(this).attr('src', $(this).attr("data-animate"));
@@ -43,4 +44,5 @@ $('.movImage').on('click', function() { //this code allows the gif to move and f
         $(this).attr('data-state', 'still');
     }
 });
+
 //code for putting the picturesd 3 per row 
